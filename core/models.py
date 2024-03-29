9b81561ac3c4df18cb2bec627e818be3d912f7af
RegisSalesRA <regismsales@gmail.com>
2024-03-29 03:40:43 -0300

    models and gitflow

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    createAt = models.DateTimeField(auto_now_add=True, auto_created=True)
+    updateAt = models.DateTimeField(auto_now=True, auto_created=True)
+
+    def __str__(self):
+        return self.name
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from core.models import Task
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Task.objects.create(name='django')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Task))
+        self.assertEqual(str(d), 'django')
