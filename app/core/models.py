6371354f009b8e154fe4ccf6ec093a05fc086a1d
RegisSalesRA <regismsales@gmail.com>
2024-03-31 23:34:24 -0300

    update commands

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 class Task(models.Model):
 
     name = models.CharField(max_length=255)
-
     createAt = models.DateTimeField(auto_now_add=True, auto_created=True)
     updateAt = models.DateTimeField(auto_now=True, auto_created=True)
 
