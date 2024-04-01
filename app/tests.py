08e2972aa64634eedc047f0a898b2feac2a916af
RegisSalesRA <regismsales@gmail.com>
2024-03-31 21:31:07 -0300

    calc test

---
+++ app/tests.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from calc import add
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = add(5,6)
+
+        self.assertEqual(res, 11)
