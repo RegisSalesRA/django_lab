259af357b056e8413148e37c5668b4ab687135ea
RegisSalesRA <regismsales@gmail.com>
2024-05-14 11:34:00 -0300

    django lab change

--- app/core/tests/test_core_views.py
+++ app/core/tests/test_core_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
+from core.models import Category, Tag, Task
 from mocks.api_routes_mock import ApiRouteMocks as routes
+from mocks.data_mock import DataMocks as dataJsonMock
 
 
 class TaskAPITests(TestCase):
@@ -9,6 +11,16 @@ class TaskAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_get_task_values(self):
+    def test_get_tasks(self):
         res = self.client.get(routes.url_api_get_tasks())
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_task_detail(self):
+        tag = Tag.objects.create(name="Tag_1")
+        category = Category.objects.create(name="Category_1")
+        task = Task.objects.create(name="Task_1", category=category)
+        task.tags.add(tag)
+
+        res = self.client.get(routes.url_api_get_task(task.id))
+    
+        self.assertEqual(res.data['name'], task.name)
--- app/mocks/api_routes_mock.py
+++ app/mocks/api_routes_mock.py
@@ -6,7 +6,13 @@ class ApiRouteInterface(ABC):
     def url_api_get_tasks():
         pass
 
+    @abstractmethod
+    def url_api_get_task():
+        pass
 
 class ApiRouteMocks(ApiRouteInterface):
     def url_api_get_tasks():
         return '/api/v1/core/tasks'
+
+    def url_api_get_task(id):
+        return f'/api/v1/core/tasks/{id}'
