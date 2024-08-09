02b7a13b4cf8c8d2720a0fd0b0751c011a4c80f4
RegisSalesRA <regismsales@gmail.com>
2024-08-09 00:20:19 -0300

    ajeitando a parte de imagens

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Category, Tag, Task
+from core.models import Category, Tag, Task, Album
 
 admin.site.register(Task)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(Album)
